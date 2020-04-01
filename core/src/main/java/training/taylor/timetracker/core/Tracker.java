c1daa6c33303f2ecd1bbd44d1ef0c1a366a14202
phgreiss <62998769+phgreiss@users.noreply.github.com>
2020-04-01 20:20:46 +0400

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -26,9 +26,7 @@ public class Tracker {
         return entries.size();
     }
 
-    public TimeEntry get(int index) {
-
-
+    public TimeEntry get(final int index) {
         return entries.get(index);
     }
 }
